3da99cfccee4ef9cbf8ed6992419ea4f11515072
Pablo Lalloni <plalloni@gmail.com>
2013-05-06 11:42:18 -0300

    Adds "log" workspace

--- xmonad.hs
+++ xmonad.hs
@@ -28,31 +28,40 @@ import qualified XMonad.StackSet as W
 modm = mod4Mask
 moda = mod1Mask
 
+baseConfig = xfceConfig
+
 main = do
-    xmonad $ xfceConfig { modMask = modm
+    xmonad $ baseConfig { modMask = modm
                         , workspaces = myWorkspaces
                         , layoutHook = myLayoutHook
                         , manageHook = myManageHook
                         } 
-                        `additionalKeys` [ ((moda,                 xK_Tab),   windows W.focusDown)
-                                         , ((modm .|. controlMask, xK_space), sendMessage NextLayout)
-                                         , ((modm,                 xK_Right), nextWS)
-                                         , ((modm,                 xK_Left),  prevWS)
-                                         , ((modm .|. shiftMask,   xK_Right), shiftToNext >> nextWS)
-                                         , ((modm .|. shiftMask,   xK_Left),  shiftToPrev >> prevWS)
-                                         , ((mod4Mask,             xK_s),     cycleRecentWindows [xK_Super_L] xK_s xK_w)
-                                         , ((modm .|. shiftMask,   xK_g),     gotoMenuArgs ["-b", "-l", "10"])
-                                         , ((modm .|. shiftMask,   xK_b),     bringMenuArgs ["-b", "-l", "10"])
-                                         , ((moda,                 xK_space), windowMenu)
-                                         , ((modm .|. controlMask, xK_x),     xmonadPrompt defaultXPConfig)
-                                         , ((modm .|. controlMask, xK_y),     shellPrompt defaultXPConfig)
-                                         , ((moda,                 xK_F4),    kill)
-                                         ]
+                        `additionalKeys` (
+                            [ ((moda,                 xK_Tab),   windows W.focusDown)
+                            , ((modm .|. controlMask, xK_space), sendMessage NextLayout)
+                            , ((modm,                 xK_Right), nextWS)
+                            , ((modm,                 xK_Left),  prevWS)
+                            , ((modm .|. shiftMask,   xK_Right), shiftToNext >> nextWS)
+                            , ((modm .|. shiftMask,   xK_Left),  shiftToPrev >> prevWS)
+                            , ((mod4Mask,             xK_s),     cycleRecentWindows [xK_Super_L] xK_s xK_w)
+                            , ((modm .|. shiftMask,   xK_g),     gotoMenuArgs ["-b", "-l", "10"])
+                            , ((modm .|. shiftMask,   xK_b),     bringMenuArgs ["-b", "-l", "10"])
+                            , ((moda,                 xK_space), windowMenu)
+                            , ((modm .|. controlMask, xK_x),     xmonadPrompt defaultXPConfig)
+                            , ((modm .|. controlMask, xK_y),     shellPrompt defaultXPConfig)
+                            , ((moda,                 xK_F4),    kill)
+                            ] 
+                            ++ 
+                            [ ((m .|. modm, k), windows $ f i) 
+                                | (i, k) <- zip myWorkspaces ([xK_1 .. xK_9] ++ [xK_0])
+                                , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
+                            ]
+                        )
                         `removeKeys` [ (modm, xK_Tab) -- win+tab
                                      , (modm, xK_space) -- reservada para kupfer
                                      ]
 
-myWorkspaces = ["dev", "mail"] ++ map show [1..6] ++ ["im"]
+myWorkspaces = ["dev", "mail"] ++ map show [1..6] ++ ["im", "log"]
 
 myLayoutHook = avoidStruts $ smartBorders $ onWorkspace "im" im $ full ||| tiled ||| Mirror tiled
     where
@@ -64,7 +73,7 @@ myManageHook = composeAll [ className =? "Pidgin" --> doShift "im"
                           , className =? "Eclipse" --> doShift "dev"
                           , className =? "Thunderbird" --> doShift "mail"
                           , className =? "Xfce4-notifyd" --> doIgnore
-                          ] <+> manageHook xfceConfig
+                          ] <+> manageHook baseConfig
 
 myTheme = TI { themeName = "myTheme"
              , theme     = defaultTheme { activeColor         = "#cccccc"
